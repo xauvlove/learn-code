51b0d915d9e1977b88670a533720d28e29725ad2
Venlexi <xauvlove@163.com>
2022-11-12 21:10:18 +0800

    avl rbtree

---
+++ main/src/main/java/com/xauv/algorithm/题目/算法实现/链表/分隔链表.java
@@ -0,0 +1,89 @@
+package com.xauv.algorithm.题目.算法实现.链表;
+/*
+       /\   /\             /\.__                      
+___  __)/___)/  __ _____  _)/|  |   _______  __ ____  
+\  \/  /\__  \ |  |  \  \/ / |  |  /  _ \  \/ // __ \ 
+ >    <  / __ \|  |  /\   /  |  |_(  <_> )   /\  ___/ 
+/__/\_ \(____  /____/  \_/   |____/\____/ \_/  \___  >
+      \/     \/                                    \/
+*/
+
+import com.xauv.algorithm.题目.数据结构.ListFactory;
+import com.xauv.algorithm.题目.数据结构.ListNode;
+
+/**
+ * @Date 2022/11/6 16:50
+ * @Author Administrator
+ * @Package com.xauv.algorithm.题目.算法实现.链表
+ * @Desc
+ */
+public class 分隔链表 {
+
+    /**
+     *
+     * 链表
+     * 1 -> 4 -> 3 -> 2 -> 5 -> 2
+     *
+     * 将 大于 3 的节点放在链表左边，大于等于 3 的节点放在链表右边
+     * 并且不改变节点的相对位置
+     *
+     * 结果为：1 -> 2 -> 2 -> 4 -> 3 -> 5
+     *
+     * 现在 1 2 2 在链表左半部分
+     * 4 3 5 在链表右半部分
+     * 并且没有改变节点之前的相对顺序
+     *
+     * 方法：
+     * 新建两个链表，遍历一次之前链表，然后用尾部插法将元素分别插入两个链表上
+     * 然后再将两个链表合并即可
+     *
+     * @param head
+     * @param val
+     * @return
+     */
+    public static ListNode divide(ListNode head, int val) {
+
+        // 新建两个虚拟头节点
+        ListNode head1 = new ListNode(0);
+        ListNode head2 = new ListNode(0);
+
+        ListNode cur1 = head1;
+        ListNode cur2 = head2;
+
+        ListNode node = head;
+
+        while (node != null) {
+            if (node.value < val) {
+                cur1.next = node;
+                cur1 = cur1.next;
+                node = node.next;
+                cur1.next = null;
+            } else {
+                cur2.next = node;
+                cur2 = cur2.next;
+                node = node.next;
+                cur2.next = null;
+            }
+        }
+        cur1.next = head2.next;
+        return head1.next;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(1);
+        ListNode n1 = new ListNode(4);
+        ListNode n2 = new ListNode(3);
+        ListNode n3 = new ListNode(2);
+        ListNode n4 = new ListNode(5);
+        ListNode n5 = new ListNode(2);
+
+        head.setNext(n1);
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+        n4.setNext(n5);
+        ListFactory.printList(head);
+        divide(head, 3);
+        ListFactory.printList(head);
+    }
+}
---
+++ main/src/main/java/com/xauv/algorithm/题目/算法实现/链表/回文链表.java
@@ -0,0 +1,171 @@
+package com.xauv.algorithm.题目.算法实现.链表;
+/*
+       /\   /\             /\.__                      
+___  __)/___)/  __ _____  _)/|  |   _______  __ ____  
+\  \/  /\__  \ |  |  \  \/ / |  |  /  _ \  \/ // __ \ 
+ >    <  / __ \|  |  /\   /  |  |_(  <_> )   /\  ___/ 
+/__/\_ \(____  /____/  \_/   |____/\____/ \_/  \___  >
+      \/     \/                                    \/
+*/
+
+import com.xauv.algorithm.题目.数据结构.ListFactory;
+import com.xauv.algorithm.题目.数据结构.ListNode;
+
+import java.util.Objects;
+
+/**
+ * @Date 2022/11/12 20:06
+ * @Author Administrator
+ * @Package com.xauv.algorithm.题目.算法实现.链表
+ * @Desc
+ */
+public class 回文链表 {
+
+    /**
+     *
+     * 判断链表是否回文链表
+     *
+     * 链表从左到右 从右到左，遍历出的元素是一样的
+     *
+     * 1 -> 2 -> 2 -> 1
+     * 是回文链表
+     *
+     * 1 -> 2 -> 3 -> 2 -> 1
+     * 是回文链表
+     *
+     * 1 -> 2
+     * 不是回文链表
+     *
+     * 思路：
+     *
+     * 例如链表
+     * 1 -> 2 -> 3 -> 2 -> 1
+     *
+     * step1.
+     * 找到中间元素 3
+     *
+     * step2.
+     * 将中间元素 3 之后的链表反转
+     * 1 -> 2 -> 3 -> 2 <- 1
+     *
+     * step3.
+     * 使用前后两个指针，分别从两端往中间移动
+     * 如果移动过程中发现元素不一致，则表示链表不是回文的
+     *
+     *
+     * notice:
+     * 如果是链表：
+     * 1 -> 2 -> 2 -> 1
+     *
+     * 则第二个元素是中间元素，
+     * 中间元素：如果链表长度为偶数，那么靠左的元素是中间元素
+     *
+     * @return
+     */
+    public static boolean isPalindrome(ListNode head) {
+        // 链表为空，链表只有 1 个节点，是回文链表
+        if (head == null || head.next == null) {
+            return true;
+        }
+        // 如果链表节点数量为 2，只需判断两者值
+        if (head.next.next == null) {
+            return Objects.equals(head.value, head.next.value);
+        }
+
+        // 1.找到中间节点
+        ListNode mid = getMidNode(head);
+        // 2.对中间节点后的链表进行反转
+        ListNode rightHead = reverse(mid.next);
+        // 3.使用双指针逐个比较元素是否相等
+        ListNode r = rightHead;
+        ListNode l = head;
+
+        // 这里不判断左指针
+        while (r != null) {
+            // 左右指针元素不相等，则表示不对称
+            if (!Objects.equals(l.value, r.value)) {
+                return false;
+            }
+            // 双端指针往中间移动
+            r = r.next;
+            l = l.next;
+        }
+
+        // 将链表再反转回去，不改变原链表
+        reverse(rightHead);
+        return true;
+    }
+
+    /**
+     * 利用快慢指针
+     *
+     * 快指针一次走两步
+     * 慢指针一次走一步
+     *
+     * 这样快指针速度是慢指针的两倍
+     *
+     * @param head
+     * @return
+     */
+    private static ListNode getMidNode(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (true) {
+            ListNode next = fast.next;
+            if (next == null) {
+                break;
+            }
+            ListNode nNext = next.next;
+            if (nNext == null) {
+                break;
+            }
+            fast = nNext;
+            slow = next;
+        }
+        return slow;
+    }
+
+    /**
+     * 反转链表
+     * @param node
+     */
+    private static ListNode reverse(ListNode node) {
+        if (node == null) {
+            return null;
+        }
+        ListNode cur = node;
+        ListNode prev = null;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
+    }
+
+    public static void main(String[] args) {
+        ListNode head = new ListNode(0);
+        ListNode n1 = new ListNode(1);
+        ListNode n2 = new ListNode(2);
+        ListNode n3 = new ListNode(3);
+        ListNode n4 = new ListNode(2);
+        ListNode n5 = new ListNode(1);
+        ListNode n6 = new ListNode(0);
+
+
+        head.setNext(n1);
+        n1.setNext(n2);
+        n2.setNext(n3);
+        n3.setNext(n4);
+        n4.setNext(n5);
+        n5.setNext(n6);
+
+        ListFactory.printList(head);
+        System.out.println(isPalindrome(head));
+    }
+}
